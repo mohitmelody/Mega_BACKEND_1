7147a1d673e07447a4fb2369fcd4657912e29840
Mohit Singh Rawat <149602310+mohitsinghrawat-developer@users.noreply.github.com>
2024-06-23 17:40:52 +0530

    Update asynchandler.js

--- src/utils/asynchandler.js
+++ src/utils/asynchandler.js
@@ -4,6 +4,8 @@ return (req, res , next) =>{
 }
 }
 export {asynchandler}
+asynchandler is generally recommended for most asynchronous route handlers in Express.js to simplify error handling and improve code readability.
+Direct mapped routes without asynchandler can be used for simple, synchronous handlers, but consider using asynchandler as your application grows and incorporates more asynchronous operations.
 // jab error aati hai tab hum message, error code bhejte hai 
 
 // 2 way
